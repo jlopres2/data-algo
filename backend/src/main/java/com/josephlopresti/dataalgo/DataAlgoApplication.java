9347318980372361ca3631bd0b73d5c1198022b2
Joseph Lopresti <jlopres2@villanova.edu>
2024-10-30 16:41:05 -0400

    Initial commit with frontend and backend setup

---
+++ backend/src/main/java/com/josephlopresti/dataalgo/DataAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.josephlopresti.dataalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAlgoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/josephlopresti/dataalgo/DataAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephlopresti.dataalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
